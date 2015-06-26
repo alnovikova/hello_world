8604f51d6c9b422c41f3502bc60ff0c8ce1f7f7e
anovikova <lire@bk.ru>
2015-06-26 13:55:37 +0600

    Synchronized update HelloWorld.java

--- src/com/luxoft/hello/HelloWorld.java
+++ src/com/luxoft/hello/HelloWorld.java
@@ -5,6 +5,6 @@ package com.luxoft.hello;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world! v1,v2");
     }
 }
