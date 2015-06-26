33c5df0a0e1f0cd955cc970a3055e25047e031db
anovikova <lire@bk.ru>
2015-06-26 13:22:15 +0600

    Added HelloWorld.java

---
+++ src/com/luxoft/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.luxoft.hello;
+
+/**
+ * Created by anovikova on 26.06.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
